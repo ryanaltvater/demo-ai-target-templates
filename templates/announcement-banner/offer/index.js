45dcbc998f86f890a326008a888200d7099ba078
Ryan Altvater <raltvate@redhat.com>
2025-06-05 13:21:59 -0400

    Initial commit

---
+++ templates/announcement-banner/offer/index.js
@@ -0,0 +1,71 @@
+(function () {
+    // Utility CSS
+    // inject:utilityCss
+    // endinject
+
+    // Custom CSS
+    // inject:customCss
+    // endinject
+
+    const audience = "";
+
+    const langs = {
+        en: {
+            paragraph: "",
+            ctaLabel: "",
+            ctaURL: ""
+        },
+        zh: {
+            paragraph: "",
+            ctaLabel: "",
+            ctaURL: ""
+        },
+        fr: {
+            paragraph: "",
+            ctaLabel: "",
+            ctaURL: ""
+        },
+        de: {
+            paragraph: "",
+            ctaLabel: "",
+            ctaURL: ""
+        },
+        it: {
+            paragraph: "",
+            ctaLabel: "",
+            ctaURL: ""
+        },
+        ja: {
+            paragraph: "",
+            ctaLabel: "",
+            ctaURL: ""
+        },
+        ko: {
+            paragraph: "",
+            ctaLabel: "",
+            ctaURL: ""
+        },
+        "pt-br": {
+            paragraph: "",
+            ctaLabel: "",
+            ctaURL: ""
+        },
+        es: {
+            paragraph: "",
+            ctaLabel: "",
+            ctaURL: ""
+        }
+    };
+
+    // Global functions
+    // inject:functions
+    // endinject
+
+    // Global scripts
+    // inject:scripts
+    // endinject
+
+    // HTML content
+    // All HTML needs to be inside of a "rh-target-activityID" wrapper class
+    // It acts as a namespace, to promote portability and prevent CSS conflicts
+})();
